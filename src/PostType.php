b4a6794cf5fad14c901b1b1cd84c8f925994e57c
Tom Gurney <=>
2023-08-02 10:54:27 +0100

    feat: add labels and show_in_rest to default create posttype

--- src/PostType.php
+++ src/PostType.php
@@ -4,12 +4,23 @@ namespace Giantpeach\Schnapps\Query;
 
 class PostType
 {
+  /**
+   * Create a custom post type
+   *
+   * @param [type] $name Singular name of the post type
+   * @param array $args
+   * @return void
+   */
   public static function create($name, $args = [])
   {
     $defaults = [
       'public' => true,
       'has_archive' => true,
       'supports' => ['title', 'editor', 'thumbnail'],
+      'show_in_rest' => true,
+      'labels' => [
+        'name' => ucfirst($name),
+      ]
     ];
 
     $args = array_merge($defaults, $args);
